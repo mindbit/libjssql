66f133419362d7277b2fa1bd8be6d9f0950f3c11
Claudiu <olteanu.claudiu@ymail.com>
2014-04-27 20:25:04 +0300

    Add unit tests for PostgreSQL simple queries

--- src/test_postgres.js
+++ src/test_postgres.js
@@ -1,11 +1,71 @@
-function foo() {
-	var conn, pstmt, rs, i = 0;
+function connection_test() {
+	var conn;
 
 	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
-	println("Connection succeeded");
-	
+	if (conn == null)
+		return "FAIL";
+	else
+		return "PASS";
+}
+
+function createStatement_test() {
+	var conn, stmt;
+
+	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	stmt = conn.createStatement();
+
+	if (stmt == null)
+		return "FAIL";
+	else
+		return "PASS";
+}
+
+function execute_test() {
+	var conn, stmt;
+
+	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	stmt = conn.createStatement();
+
+	if (stmt.execute("BEGIN") == false)
+		return "FAIL";
+	else
+		return "PASS";
+}
+
+function executeQuery_test() {
+	var conn, stmt;
+
+	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	stmt = conn.createStatement();
+
+	result = stmt.executeQuery("select * from information_schema.tables");
+
+	if (result == null)
+		return "FAIL";
+	else
+		return "PASS";
+}
+
+function executeUpdate_test() {
+	var conn, stmt;
+
+	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	stmt = conn.createStatement();
+
+	ret = stmt.executeUpdate("BEGIN");
+	if (ret != 0)
+		return "FAIL";
+	else
+		return "PASS";
+}
+
+function foo() {
+	println("[Test 1] Testing connection .................. " + connection_test());
+	println("[Test 2] Testing createStatement ............. " + createStatement_test());
+	println("[Test 3] Testing execute ..................... " + execute_test());
+	println("[Test 4] Testing executeQuery ................ " + executeQuery_test());
+	println("[Test 5] Testing executeUpdate ............... " + executeUpdate_test());
 	return 0;
 }
 
-dump("12ășț45");
 foo();
