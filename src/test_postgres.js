8d2522fca68bca61ded8fe31ccd3eac3ab80c3e0
Claudiu <olteanu.claudiu@ymail.com>
2014-05-06 22:34:58 +0300

    Update the tests using the new returned values

--- src/test_postgres.js
+++ src/test_postgres.js
@@ -12,8 +12,10 @@ function createStatement_test() {
 	var conn, stmt;
 
 	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	if (conn == null)
+		return "FAIL";
+	
 	stmt = conn.createStatement();
-
 	if (stmt == null)
 		return "FAIL";
 	else
@@ -24,6 +26,9 @@ function execute_test() {
 	var conn, stmt;
 
 	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	if (conn == null)
+		return "FAIL";
+	
 	stmt = conn.createStatement();
 
 	if (stmt.execute("BEGIN") == false)
@@ -36,10 +41,14 @@ function executeQuery_test() {
 	var conn, stmt;
 
 	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	if (conn == null)
+		return "FAIL";
+	
 	stmt = conn.createStatement();
+	if(stmt == null)
+		return "FAIL";
 
 	result = stmt.executeQuery("select * from information_schema.tables");
-
 	if (result == null)
 		return "FAIL";
 	else
@@ -50,21 +59,29 @@ function executeUpdate_test() {
 	var conn, stmt;
 
 	conn = DriverManager.getConnection("postgresql://127.0.0.1/licenta", "claudiu", "1234%asd");
+	if (conn == null)
+		return "FAIL";
+	
 	stmt = conn.createStatement();
-
+	if (stmt == null)
+		return "FAIL";
+	
 	ret = stmt.executeUpdate("BEGIN");
-	if (ret != 0)
+	if (ret == -1)
 		return "FAIL";
 	else
 		return "PASS";
 }
 
+
+
 function foo() {
-	println("[Test 1] Testing connection .................. " + connection_test());
-	println("[Test 2] Testing createStatement ............. " + createStatement_test());
-	println("[Test 3] Testing execute ..................... " + execute_test());
-	println("[Test 4] Testing executeQuery ................ " + executeQuery_test());
-	println("[Test 5] Testing executeUpdate ............... " + executeUpdate_test());
+	println("[Test 1] Testing connection ......................................... " + connection_test());
+	println("[Test 2] Testing createStatement .................................... " + createStatement_test());
+	println("[Test 3] Testing execute for simple statement ....................... " + execute_test());
+	println("[Test 4] Testing executeQuery for simple statement .................. " + executeQuery_test());
+	println("[Test 5] Testing executeUpdate for simple statement ................. " + executeUpdate_test());
+
 	return 0;
 }
 
